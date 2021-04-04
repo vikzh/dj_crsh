5748dea9a787ce63d22277b311b2cf65b0d99aa3
vikzh <vikt.anmail@gmail.com>
2021-04-04 14:02:07 +0300

    creator field added

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -3,6 +3,8 @@ from model_utils.models import TimeStampedModel
 from autoslug import AutoSlugField
 from django_countries.fields import CountryField
 from django.urls import reverse
+from django.conf import settings
+
 
 
 class Cheese(TimeStampedModel):
@@ -10,6 +12,9 @@ class Cheese(TimeStampedModel):
     slug = AutoSlugField("Cheese Address", unique=True, always_update=False, populate_from="name")
     description = models.TextField("Description", blank=True)
     country_of_origin = CountryField("Country of Origin", blank=True)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                null=True,
+                                on_delete=models.SET_NULL)
 
     def get_absolute_url(self):
         return reverse('cheeses:detail',
